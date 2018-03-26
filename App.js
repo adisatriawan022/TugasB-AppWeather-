a40f27ce3b6848a15cadeef79ec5f49e0e17c40e
adisatriawan022 <37111842+adisatriawan022@users.noreply.github.com>
2018-03-26 23:24:10 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AppRegistry, StyleSheet, View, Text, TextInput, Button, Image} from 'react-native';
+
+import Layout from './src/Comp/Weather2';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Layout />
+    );
+  }
+}
